78a3b6e59d74e81625b97bf25db610f9f238c0e0
DeadSage <79532594+DeadSage@users.noreply.github.com>
2021-10-02 15:33:08 +0300

    moving alien fleet left/right

--- alien_invasion.py
+++ alien_invasion.py
@@ -55,14 +55,33 @@ class AlienInvasion:
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
 
+    def _check_fleet_edges(self):
+        """Реагирует на достижение пришельцем края экрана."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self.change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Опускает весь флот и меняет направление флота."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
     def run_game(self):
         """Запуск основного цикла игры."""
         while True:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
         # При каждом проходе цикла перерисовывается экран.
+
+    def _update_aliens(self):
+        """Обновляет позиции всех пришельцев во флоте."""
+        self._check_fleet_edges()
+        self.aliens.update()
 
     def _check_events(self):
         """Обрабатывает нажатия клавиш и события мыши."""
